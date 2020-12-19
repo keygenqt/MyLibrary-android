f5cd199705f02c4394b942fc1605207395e5ae8a
keygenqt <keygenqt@gmail.com>
2020-12-19 15:35:38 +0300

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.keygenqt.mylibrary"
         minSdkVersion(24)
         targetSdkVersion(30)
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
